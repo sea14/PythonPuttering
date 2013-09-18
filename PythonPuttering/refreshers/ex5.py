ddfcc51ffe75b8ca94a32573731816106065b836
sea14 <pianocat14@gmail.com>
2013-09-17 21:03:10 -0400

    more silly review examples

---
+++ PythonPuttering/refreshers/ex5.py
@@ -0,0 +1,10 @@
+my_name = 'Slim Shady' #because I had to.
+my_age = 40
+my_height = 74
+
+print "My name is %s." % my_name #the percent sign with an s links a variable into the print phrase
+
+#need to remember the % links things, including parameters
+print "My height is %s inches and my age is %s years." % (my_height, my_age)
+
+
---
+++ PythonPuttering/refreshers/ex6.py
@@ -0,0 +1,7 @@
+x = "Coheed and Cambria has %d albums." % 7
+current_sound = "'In Keeping Secrets of Silent Earth: 3'"
+next_album = "'Good Apollo I'm Burning Star V: Volume I'"
+
+y = "The album I'm listening to now is %s. %s is the next one in their discography." % (current_sound, next_album)
+
+print y
