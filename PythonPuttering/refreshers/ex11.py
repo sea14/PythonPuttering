56171e1f2f2fadb6f3652c02d0b5839a83178137
sea14 <pianocat14@gmail.com>
2013-09-17 21:25:21 -0400

    more examples and refreshing

---
+++ PythonPuttering/refreshers/ex11.py
@@ -0,0 +1,4 @@
+print "How old are you?"
+age = raw_input()
+
+print "So, you're %r years old." % (age)
---
+++ PythonPuttering/refreshers/ex12.py
@@ -0,0 +1,4 @@
+#another way of doing the previous example. yay?
+age = raw_input("How old are you? ")
+
+print "You are %r years old." % (age)
